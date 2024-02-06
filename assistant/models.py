ffd8e0569b78e236041c748d9ba38c4a681e0c16
Fabio Solidade <solidadefab@gmail.com>
2024-02-06 15:52:56 -0300

    models in test

--- assistant/models.py
+++ assistant/models.py
@@ -1,10 +1,19 @@
+# assistant/models.py
+
 from django.db import models
 
-class ItemCardapio(models.Model):
-    categoria = models.CharField(max_length=50)
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField(blank=True, null=True)
-    preco = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Comida(models.Model):
+    comida = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.nome
+        return self.comida
+
+class Carrinho(models.Model):
+    token = models.CharField(max_length=255)
+    comida = models.ForeignKey(Comida, on_delete=models.CASCADE)
+    quantidade = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ('token', 'comida')
+
