ed1583908ea802bd4624bcd31f9e95d16d59a310
Fabio Solidade <solidadefab@gmail.com>
2024-02-09 08:27:53 -0300

    register admin

--- assistant/admin.py
+++ assistant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Attribute, AttributeOption, CustomizedItem
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(AttributeOption)
+admin.site.register(CustomizedItem)
